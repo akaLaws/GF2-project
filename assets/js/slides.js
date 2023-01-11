ebd499b838cb6c8155ea41e326eb34c37c34472f
akaLaws <sel@rts.dk>
2023-01-11 13:31:47 +0100

    lazy staging - did it all

---
+++ assets/js/slides.js
@@ -0,0 +1,39 @@
+// The slide array containing the slide elements as objects
+// Each object conteains a header, a text, a button link, a button text, a background image ande any individual styling. 
+
+const SLIDE_ARRAY = [
+   
+    // SLIDE_ARRAY[0]
+    {
+        heading: "Lorem Ipsum",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget nunc diam.",
+        buttonLink: "#",
+        buttonText: "Mitto",
+        slideImage: "/assets/images/worker.jpg",
+        slideStyle: ""
+
+
+    },
+   
+    // SLIDE_ARRAY[1]
+    {
+        heading: "Morbu nec",
+        text: "Duis in libro sed auge semper pharetra sed sit amet erat.",
+        buttonLink: "#",
+        buttonText: "Mitto",
+        slideImage: "/assets/images/worker-mask.jpg",
+        slideStyle: ""
+
+    },
+    // SLIDE_ARRAY[2]
+    {
+        heading: "Fusce bibendum cursus bibendum",
+        text: "Mauris viverra molis lorem a luctus. Vivamus malesuada, metus lacinia semper consequat, nibh arcu egestas nisl, at egestas quam nisl quis odio.",
+        buttonLink: "#",
+        buttonText: "Mitto",
+        slideImage: "/assets/images/worker-white-canvas.jpg",
+        slideStyle: ""
+
+    }
+
+];
